fb423ec9927bf7721430ce411cb3c144cc63cc96
Jon Whitter <me@jonwhitter.com>
2024-12-01 19:46:04 +0000

    little bit of a tidy

--- AdventOfCode2024/Solvers/Day1Solver.cs
+++ AdventOfCode2024/Solvers/Day1Solver.cs
@@ -12,12 +12,12 @@ public class Day1Solver : BaseSolver<(int[] First, int[] Second), int>
     }
 
     public override int SolvePart1((int[] First, int[] Second) inputData)
-        => inputData.First.Zip(inputData.Second, (first, second) => Math.Abs(first - second)).Sum();
+        => Enumerable.Zip(inputData.First, inputData.Second, (first, second) => Math.Abs(first - second)).Sum();
 
     public override int SolvePart2((int[] First, int[] Second) inputData)
     {
         var counts = inputData.Second.CountBy(x => x).ToDictionary();
 
-        return inputData.First.Select(x => x * counts.GetValueOrDefault(x)).Sum();
+        return inputData.First.Sum(x => x * counts.GetValueOrDefault(x));
     }
 }
